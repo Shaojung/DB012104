dfb2e30d838e952c8fd0c4098224dca630091dcd
shaojung <shaojung@gmail.com>
2017-01-21 15:22:44 +0800

    use ItemId

--- app/src/main/java/com/example/student/db012104/MainActivity.java
+++ app/src/main/java/com/example/student/db012104/MainActivity.java
@@ -16,19 +16,28 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add("Settings");
-        menu.add("AABB");
-        menu.add("About");
+        menu.add(0, 0, 0, R.string.action_settings);
+        menu.add(0, 1, 1, R.string.action_aabb);
+        menu.add(0, 2, 2, R.string.action_about);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getTitle().toString().equals("AABB"))
+        if (item.getItemId() == 0)
+        {
+            Toast.makeText(MainActivity.this, "Settings被按下", Toast.LENGTH_SHORT).show();
+        }
+
+        if (item.getItemId() == 1)
         {
             Toast.makeText(MainActivity.this, "AABB被按下", Toast.LENGTH_SHORT).show();
         }
 
+        if (item.getItemId() == 2)
+        {
+            Toast.makeText(MainActivity.this, "About被按下", Toast.LENGTH_SHORT).show();
+        }
         return super.onOptionsItemSelected(item);
     }
 }
